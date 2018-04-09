73aba2c49dea96ca26ed88ce60274c4bff763927
Lonami Exo <totufals@hotmail.com>
2018-04-09 12:34:56 +0200

    Add CLI arguments for size and verbosity

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 extern crate rand;
 extern crate image;
+extern crate argparse;
 
 use std::fs::File;
+use std::str::FromStr;
+use std::fmt::Display;
+use std::process::exit;
 
 use rand::{Rng, thread_rng};
 use image::ImageBuffer;
+use argparse::{ArgumentParser, StoreTrue, Store};
 
 fn offset(value: u8) -> u8 {
     let mut random: i32 = 0;
@@ -38,23 +43,57 @@ fn neighbours(x: usize, y: usize, w: usize, h: usize)
     result
 }
 
+fn parse_or_exit<T>(what: &str, name: &str) -> T
+    where
+        T: FromStr,
+        <T as FromStr>::Err: Display
+{
+    match what.parse::<T>() {
+        Ok(x) => x,
+        Err(e) => {
+            eprintln!("Could not parse {} into a number ({})", name, e);
+            exit(1);
+        }
+    }
+}
+
 fn main() {
-    const W: usize = 500;
-    const H: usize = 500;
-    let mut img = ImageBuffer::new(W as u32, H as u32);
-    let mut added = [[false; W]; H];
+    let mut verbose = false;
+    let mut size = "500x500".to_string();
+    {
+        let mut ap = ArgumentParser::new();
+        ap.set_description("Create a new fondo.");
+        ap.refer(&mut verbose)
+            .add_option(&["-v", "--verbose"], StoreTrue,
+            "Be verbose");
+        ap.refer(&mut size)
+            .add_option(&["-s", "--size"], Store,
+            "Size for the generated image in WxH format");
+        ap.parse_args_or_exit();
+    }
+
+    let size: Vec<&str> = size.split('x').collect();
+    if size.len() != 2 {
+        eprintln!("Incorrect size format (must be WxH)");
+        exit(1);
+    }
+    let w: usize = parse_or_exit(size[0], "width");
+    let h: usize = parse_or_exit(size[1], "height");
+
+    let mut img = ImageBuffer::new(w as u32, h as u32);
+    let mut added = vec![vec![false; w]; h];
     let mut pending = Vec::new();
 
-    let start_points = vec![(W / 2, H / 2)];
+    let start_points = vec![(w / 2, h / 2)];
     for (x, y) in start_points {
-        pending.push((0u8, 0u8, 0u8, W / 2, H / 2));
+        pending.push((0u8, 0u8, 0u8, w / 2, h / 2));
         added[y][x] = true;
     }
 
-    let total = W * H;
+    let total = w * h;
     let mut done = 0;
     while !pending.is_empty() {
-        if done % 10_000 == 0 {
+        if verbose && done % 10_000 == 0 {
             println!("{:.2}%", 100.0 * (done as f64 / total as f64));
         }
 
@@ -67,7 +106,7 @@ fn main() {
 
         img.put_pixel(x as u32, y as u32, image::Rgb([r, g, b]));
         done += 1;
-        for &(x, y) in neighbours(x, y, W, H).iter() {
+        for &(x, y) in neighbours(x, y, w, h).iter() {
             if !added[y][x] {
                 pending.push((r, g, b, x, y));
                 added[y][x] = true; // Moving this outside makes it more sparse
@@ -75,8 +114,12 @@ fn main() {
         }
     }
 
-    println!("100.00%. Saving...");
+    if verbose {
+        println!("100.00%. Saving...");
+    }
     let ref mut fp = File::create("ass.png").unwrap();
     image::ImageRgb8(img).save(fp, image::PNG).unwrap();
-    println!("Done.");
+    if verbose {
+        println!("Done.");
+    }
 }
